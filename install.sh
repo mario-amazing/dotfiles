a42b074c6e4380385008aca0d9b230206cc25a20
megosparta <megosparta@mail.ru>
2015-07-08 21:01:31 +0300

    Added adobe flash

--- install.sh
+++ install.sh
@@ -67,6 +67,7 @@ install_programs(){
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 install_pachages google-chrome-stable
+install_pachages flashplugin-installer
 }
 
 install_git
