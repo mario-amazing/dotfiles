28606fc79220ef5550f0a50fe1d4acb290ffd5dc
mario-amazing <marian1amazing@gmail.com>
2019-12-25 18:00:38 +0300

    Config paspberry

---
+++ raspberry/ubuntu_install.bash
@@ -0,0 +1,230 @@
+#TODO  settings, su, sudo update after install
+# SELF_DIR="$(dirname $0)"
+SELF_DIR=`realpath $(dirname $BASH_SOURCE)`
+ROOT_DIR=`dirname "$SELF_DIR"`
+
+copy(){
+  yes | cp -f $@
+}
+
+install_pachages() {
+  for i in $@; do
+    sudo apt install -y --force-yes $i
+  done
+}
+
+remove_pachages(){
+  for i in $@; do
+    sudo apt purge -y $i
+  done
+}
+
+init_settings() {
+  sudo add-apt-repository -y "deb http://archive.canonical.com/ubuntu $(lsb_release -sc) partner"
+  sudo add-apt-repository -y "deb-src http://archive.canonical.com/ubuntu $(lsb_release -sc) partner"
+  install_pachages curl
+}
+
+install_git() {
+  install_pachages git
+  git config --global color.ui true
+  git config --global user.name  "$1"
+  git config --global user.email "$2"
+  git config --global push.default current
+  git config --global core.excludesfile ~/.gitignore_global
+  git config --global help.autoCorrect -1
+  copy $SELF_DIR/gitignore_global $HOME/.gitignore_global
+}
+
+install_heroku() {
+  wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+}
+
+install_tmux() {
+  copy "$SELF_DIR/tmux.conf" "$HOME/.tmux.conf"
+}
+
+install_vim() {
+  sudo add-apt-repository -y ppa:neovim-ppa/unstable
+  sudo apt-get update
+  install_pachages vim-gnome neovim
+  install_pachages python-neovim python3-neovim python-pip python3-pip python-pip python-dev python3-dev
+  install_pachages software-properties-common exuberant-ctags
+  curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  mkdir $HOME/.vim -p
+  copy -r $SELF_DIR/vim/vim/* $HOME/.vim
+  copy $SELF_DIR/vim/vimrc $HOME/.vimrc
+  mkdir ~/.config
+  ln -s ~/.vim ~/.config/nvim
+  ln -s ~/.vimrc ~/.config/nvim/init.vim
+  yes | vim +PlugInstall
+}
+
+install_fonts() {
+  mkdir $HOME/.fonts -p
+  copy -r $SELF_DIR/fonts/* $HOME/.fonts
+  fc-cache
+}
+
+install_zsh() {
+  install_pachages zsh
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+  mkdir -p "$HOME/.zsh"
+  ln -vsf "$ROOT_DIR/zsh/raspberry/zshrc" "$HOME/.zshrc"
+  ln -vsf "$ROOT_DIR/zsh/raspberry/aliases.zsh" "$HOME/.zsh/aliases.zsh"
+
+  chsh -s `which zsh`
+}
+
+install_terminator() {
+  sudo add-apt-repository -y ppa:gnome-terminator/nightly
+  sudo apt-get update ; install_pachages terminator
+  mkdir $HOME/.config/terminator -p
+  copy $SELF_DIR/terminator/config $HOME/.config/terminator/
+}
+
+install_term_colors() {
+  cd /tmp
+  local target_dir=base16
+  git clone https://github.com/chriskempson/base16-builder.git "$target_dir"
+  cd "$target_dir"
+  ./base16
+  bash "chmod +x output/gnome-terminal/base16-eighties.dark.sh"
+  bash "./output/gnome-terminal/base16-eighties.dark.sh"
+  cd "$SELF_DIR"
+}
+
+install_ruby() {
+  ln -vsf "$ROOT_DIR/pry/pryrc" "$HOME/.pryrc"
+  ln -vsf "$ROOT_DIR/pry/pryrc_helpers.rb" "$HOME/.pryrc_helpers.rb"
+  ln -vsf "$ROOT_DIR/irbrc" "$HOME/.irbrc"
+  ln -vsf "$ROOT_DIR/gemspec_template" "$HOME/.gemspec_template"
+  ln -vsf "$ROOT_DIR/gemrc" "$HOME/.gemrc"
+
+  install_pachages curl g++, gcc, autoconf, automake, bison, libc6-dev, libffi-dev, libgdbm-dev, libncurses5-dev, libsqlite3-dev, libtool, libyaml-dev, make, pkg-config, sqlite3, zlib1g-dev, libgmp-dev, libreadline-dev, libssl-dev
+  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+  curl -sSL https://get.rvm.io | bash -s stable
+  source ~/.rvm/scripts/rvm
+  rvm install 2.6
+  rvm --default use ruby-2.6
+}
+
+install_gems() {
+  gem install bundler
+  gem install pry
+  gem install pry-rails
+  gem install interactive_editor
+  gem install awesome_print
+  gem install neovim
+  gem install rmagick
+  gem install shutup
+}
+
+install_numix(){
+  sudo add-apt-repository -y ppa:numix/ppa
+  sudo apt-get update ; install_pachages numix-gtk-theme numix-icon-theme-circle numix-wallpaper-*
+  install_pachages unity-tweak-tool gnome-tweak-tool
+}
+
+install_programs(){
+  install_pachages ubuntu-desktop
+	install_pachages postgresql postgresql-contrib
+  # install_pachages htop
+  # install_pachages ubuntu-restricted-extras   #Flash,java,audio-,video-codecs
+  # install_pachages chromium-browser pepperflashplugin-nonfree
+  # install_pachages sni-qt:i386 libdbusmenu-qt2:i386 libqt4-dbus:i386 libxss1:i386 libasound2-plugins:i386
+  # install_pachages skype
+  # install_pachages vlc krita  #Video-player and paint
+  # install_pachages clementine
+  # install_pachages dconf-tools  #configs
+  # install_pachages preload  #cache the most used programs
+  # install_pachages nodejs
+  # install_pachages npm
+  # install_pachages ncurses-dev #libs-dev
+  # install_pachages libpq-dev
+  # install_pachages silversearcher-ag #ag to ctrl-p plagin
+  # install_pachages imagemagick libmagickwand-dev
+  # cd /tmp
+
+  # install_pachages gdebi
+  # wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+  # sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+  # sudo apt-get update ; install_pachages google-chrome-stable
+
+  # wget http://de.archive.ubuntu.com/ubuntu/pool/universe/q/qtmobility/libqtmultimediakit1_1.2.0-1ubuntu2_amd64.deb  #screen window
+  # sudo dpkg -i libqtmultimediakit1_1.2.0-1ubuntu2_amd64.deb
+  # sudo apt-get install -f
+  # wget http://download.opensuse.org/repositories/home:olav-st/xUbuntu_14.04/Release.key | sudo apt-key add -
+  # sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/olav-st/xUbuntu_14.04/ /' > /etc/apt/sources.list.d/screencloud.list"
+  # sudo apt-get update ; install_pachages screencloud
+
+  # sudo add-apt-repository -y ppa:nilarimogard/webupd8   #Equalizer for fix audio
+  # sudo apt-get update ; install_pachages pulseaudio-equalizer
+
+  # sudo add-apt-repository -y ppa:linrunner/tlp    #save energy
+  # sudo apt-get update ; install_pachages tlp tlp-rdw
+  # sudo tlp start
+
+  # sudo dpkg --add-architecture i386 ; sudo apt-get update #teamviewer
+  # install_pachages libdbus-1-3:i386 libasound2:i386 libexpat1:i386 libfontconfig1:i386 libfreetype6:i386 libjpeg62:i386 libpng12-0:i386 libsm6:i386 libxdamage1:i386 libxext6:i386 libxfixes3:i386 libxinerama1:i386 libxrandr2:i386 libxrender1:i386 libxtst6:i386 zlib1g:i386 libc6:i386
+  # wget http://download.teamviewer.com/download/teamviewer_i386.deb
+  # yes | sudo gdebi teamviewer*.deb
+  # sudo dpkg --remove-architecture i386
+  # cd "$SELF_DIR"
+}
+
+install_steam() {
+  install_pachages steam
+  sudo add-apt-repository -y ppa:xorg-edgers/ppa
+  install_pachages xserver-xorg-core xserver-xorg-video-intel libcheese7 libcheese-gtk23 libclutter-1.0-0 libclutter-gtk-1.0-0 libcogl15 libclutter-gst-2.0-0 gstreamer1.0-clutter     #libs for font
+}
+
+fix_logs(){
+  sudo bash -c "echo 'vm.swappiness=0'>> /etc/sysctl.conf"
+  sudo bash -c "echo 'SUSPEND_MODULES="xhci-hcd"'>> /etc/pm/config.d/unload_module"
+  sudo bash -c "echo 'blacklist btusb'>> /etc/modprobe.d/blacklist.conf"
+  gsettings set com.canonical.desktop.interface scrollbar-mode normal  #Normal scroll
+  gsettings set org.gnome.settings-daemon.plugins.power button-power shutdown
+  gsettings set org.gnome.settings-daemon.plugins.power critical-battery-action nothing
+  gsettings set org.gnome.settings-daemon.plugins.power percentage-low 25
+  gsettings set org.gnome.settings-daemon.plugins.power percentage-critical 5
+  gsettings set org.compiz.integrated show-hud "['']"
+  gsettings set org.gnome.settings-daemon.plugins.media-keys terminator "<Alt>t"
+}
+
+remove_programs(){
+  remove_pachages zeitgeist zeitgeist-core zeitgeist-datahub gnome-orca unity-webapps-common
+  remove_pachages rhythmbox totem totem-common
+  remove_pachages empathy empathy-common nautilus-sendto-empathy  #center fast message
+  # remove_pachages thunderbird
+}
+
+tmp_configs() {
+  sudo bash -c "echo '127.0.0.1  localhost www1.centerv.by www.centerv.by crm1.centerv.by centerv.by centerv.by.local'>> /etc/hosts"
+}
+
+finish_fix() {
+  sudo apt-get autoremove -y
+}
+
+# init_settings
+# install_vim
+# install_steam
+# install_git
+# install_fonts
+# install_heroku
+# install_gems
+# install_numix
+# install_term_colors
+# install_terminator
+# remove_programs
+# fix_logs
+# tmp_configs
+# finish_fix
+
+# Fixed
+# install_programs
+# With reboot
+# install_ruby
+# install_zsh
---
+++ zsh/raspberry/aliases.zsh
@@ -0,0 +1,139 @@
+# Common
+alias vim='vim'
+alias be='bundle exec'
+alias x='exit'
+alias v='nvim'
+alias sv="sudo nvim"
+alias s='subl'
+alias t='touch'
+alias md='mkdir'
+alias g='gedit'
+alias p='pry'
+# alias mysql='mysql --auto-rehash'
+alias kl='kill -9'
+alias o='open'
+
+#Ctags
+# alias ctg='ctags -R --exclude=.git --exclude=log --exclude=*.js -f tmp/tags *'
+# alias ctg="ctags -R --languages=ruby --exclude=.git --exclude=log --exclude='*.js' `bundle show --paths` ."
+alias ctg="ctags -R --languages=ruby --exclude=.git --exclude=log --exclude='*.js' ."
+alias rtg="ripper-tags -R --extra=q"
+
+alias ls='ls -G'
+alias sk='sudo kill -9'
+
+# alias ls='ls --color=auto'
+
+# Git
+alias gi='git init'
+alias ga='git add'
+alias gc='git commit -m'
+alias gca='git commit --amend'
+alias gce='git commit --amend --no-edit'
+alias gcam='git commit --amend -m'
+alias gcmf='git commit -m "Minor fix"'
+alias gcmfs='git commit -m "Minor fixes"'
+alias gp='git push'
+alias gs='git status -s'
+alias gcl='git clone'
+alias gpl='git pull --rebase'
+
+alias gsh='git stash'
+alias gsha="git stash apply"
+alias gshl="git stash list"
+alias gshc="git stash clear"
+
+alias gco='git checkout'
+alias gcom='git checkout master'
+alias gcod='git checkout develop'
+alias gd='git diff'
+
+alias grba='git rebase --abort'
+alias grbc='git rebase --continue'
+alias grb='git rebase'
+alias grbm='git rebase master'
+grbih () { git rebase -i HEAD~$1 ;}
+
+alias grs='git reset'
+alias gcn='git clean -di'
+
+alias gplu='git pull upstream'
+alias gplum='git pull upstream master'
+alias gplud='git pull upstream develop'
+alias gplom='git pull origin master'
+alias gplod='git pull origin develop'
+alias gplrbm='git checkout master && git pull --rebase && git checkout - && git rebase master'
+
+alias gpum='git push upstream master'
+alias gpud='git push upstream develop'
+alias gpom='git push origin master'
+alias gpod='git push origin develop'
+
+alias gl="git log --date-order --date=iso --graph --full-history --all --pretty=format:'%x08%x09%C(red)%h %C(cyan)%ad%x08%x08%x08%x08%x08%x08%x08%x08%x08%x08%x08%x08%x08%x08%x08 %C(bold blue)%<(10)%aN%C(reset) %C(reset)%<(70,trunc)%s%C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset) '"
+
+alias ss="spring stop && pkill spring"
+
+# alias rg='bundle exec rails g'
+alias rgi="bundle exec rails generate i18n"
+alias rgsc="bundle exec rails generate scaffold"
+alias rgmg="bundle exec rails generate migration"
+alias rgmo="bundle exec rails generate model"
+alias rgma="bundle exec rails generate mailer"
+alias rgco="bundle exec rails generate controller"
+alias rgo="bundle exec rails generate observer"
+
+alias drgsc="bundle exec rails d scaffold"
+alias drgmg="bundle exec rails d migration"
+alias drgmo="bundle exec rails d model"
+alias drgma="bundle exec rails d mailer"
+alias drgco="bundle exec rails d controller"
+
+alias sudo apt-get install='sudo apt-get install -y'
+alias sudo apt-get add-apt-repository='sudo apt-get add-apt-repository -y'
+alias f=fg
+alias si='sudo apt-get install -y'
+alias sp='sudo apt-get purge'
+alias bi='bundle init --gemspec=~/.gemspec_template'
+alias bl='bundle install'
+alias bgi='gem install'
+
+alias ez="vim ~/.zshrc"
+alias ea="vim ~/.zsh/aliases.zsh"
+alias ev="vim ~/.vimrc"
+alias ep="vim ~/.pryrc"
+alias ee="vim .env"
+alias eph="vim ~/.pryrc_helpers.rb"
+
+alias rc='bundle exec rails c'
+alias drc='dotenv bundle exec rails c'
+alias rs='bundle exec rails s'
+
+alias rdb='bundle exec rake db:migrate'
+alias rdbr='bundle exec rake db:rollback'
+alias rr='bundle exec rake routes'
+alias rrg='bundle exec rake routes | grep'
+alias rs2='ruby script/server -u'
+alias rc2='ruby script/console'
+alias be='bundle exec'
+alias re='bundle exec rspec'
+alias rf='bundle exec rspec --only-failures'
+alias bre='bundle exec bin/rspec'
+
+#go
+alias gor='go run'
+
+#start WORK
+WORK_PATH='/Users/mario_amazing/work/itterritory'
+alias cdw='cd $WORK_PATH'
+alias cdd='cd ~/Documents/dotfiles'
+
+alias cdpt='cd $WORK_PATH/ptero'
+alias cdpts='cd $WORK_PATH/ptero-share'
+alias cdpr='cd $WORK_PATH/promo'
+alias cdh='cd $WORK_PATH/helpdesk'
+# alias run_fidor='osascript $WORK_PATH/fidor/scripts/fidor_services.scpt'
+
+#overmind
+alias oc='overmind connect'
+alias ok='overmind kill'
+alias or='overmind restart'
